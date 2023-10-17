697252322719b3876f98552fbaebec5e68b7c0f8
Nilaxsh <nilaxsakana@gmail.com>
2023-10-17 15:48:07 +0530

    js file added

---
+++ script.js
@@ -0,0 +1,64 @@
+
+//day 1
+//Comments can make code readable
+//Welcome to learn Javascript
+/*comments can make code readable, easy to reuse and informative*/
+
+console.log(typeof myString); // Output: string
+console.log(typeof isTrue);   // Output: boolean
+console.log(typeof myUndefined);
+console.log(typeof myNull);    // Output: object (This is a JavaScript quirk)
+
+//q4
+
+
+//q5
+console.log(variable1); // Output: undefined
+console.log(variable2); // Output: undefined
+console.log(variable3); // Output: undefined
+console.log(variable4); // Output: undefined
+
+//q6
+var variable1 = 10;
+var variable2 = "Hello";
+var variable3 = true;
+var variable4 = null;
+
+console.log(variable1); // Output: 10
+console.log(variable2); // Output: Hello
+console.log(variable3); // Output: true
+console.log(variable4); // Output: nul
+
+//q7
+var variable1 = 42;
+var variable2 = "OpenAI";
+var variable3 = false;
+var variable4 = [1, 2, 3];
+
+console.log(variable1); // Output: 42
+console.log(variable2); // Output: OpenAI
+console.log(variable3); // Output: false
+console.log(variable4); // Output: [1, 2, 3]
+
+//q8
+ var firstName = "nilax";
+ var lastName = "kanesh";
+ var maritalStatus = "Single";
+ var country = "srilanka";
+ var age = 24;
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Marital Status:", maritalStatus);
+console.log("Country:", country);
+console.log("Age:", age);
+
+//q9
+var firstName = "nilax", lastName = "kanesh", maritalStatus = "Single", country = "srilanka", age = 24;
+
+
+//q10
+let myAge = 25;
+let yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
